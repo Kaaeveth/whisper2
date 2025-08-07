397cae293298f12553aee69671c3016ddb6d083e
Dominik Strutz <kaaeveth@bofloos.de>
2025-08-07 17:31:31 +0200

    fix: Check model path
    - Properly check if the selected Ollama models directory exists

--- src-tauri/src/commands/ollama_commands.rs
+++ src-tauri/src/commands/ollama_commands.rs
@@ -1,6 +1,7 @@
 use std::path::PathBuf;
 
-use tauri::State;
+use log::trace;
+use tauri::{ipc::Channel, State};
 
 use crate::{backend::{ollama::{not_ollama, OllamaBackend, OllamaPullProgress, OLLAMA_NAME}, BackendStore}, commands::backend_commands::get_backend, errors::{self, Error}, settings::AppSettings, with_llm};
 
@@ -51,10 +52,43 @@ pub async fn ollama_set_models_path(
     with_llm!(OLLAMA_NAME, &store, write|backend {
         let ollama = backend.to_mut::<OllamaBackend>().ok_or(not_ollama())?;
         let path = PathBuf::from(path);
-        let _ = path.try_exists()?;
+        if !path.exists() {
+            return Err(Error::Io(std::io::Error::new(std::io::ErrorKind::NotFound, "Models directory does not exist")));
+        }
 
         ollama.set_models_path(&path).await?;
         settings.read().await.store_ollama_models_path(&path);
+        Ok(())
+    })
+}
+
+#[tauri::command]
+pub async fn ollama_pull_model(
+    tag: String,
+    progress_channel: Channel<OllamaPullProgress>,
+    store: State<'_, BackendStore>,
+)
+-> Result<(), errors::Error>
+{
+    with_llm!(OLLAMA_NAME, &store, write|backend {
+        let ollama = backend.to_mut::<OllamaBackend>().ok_or(not_ollama())?;
+        let mut progress_receiver = ollama.pull_model(&tag).await?;
+        tokio::spawn(async move {
+            let mut iter: u64 = 0;
+            while let Some(progress) = progress_receiver.recv().await {
+                if iter % 64 == 0 || progress.status == "success" || progress.error.is_some() {
+                    trace!("Pulling {tag} - {:?}", progress);
+                    if let Err(e) = progress_channel.send(progress) {
+                        trace!("Pulling model - Channel closed: {:?}", e);
+                        break;
+                    }
+                }
+                iter += 1;
+            }
+            let _ = progress_channel.send(OllamaPullProgress {
+                status: "done".into(), digest: None, total: None, completed: None, error: None
+            });
+        });
         Ok(())
     })
 }
